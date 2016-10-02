d395186494c9734e17e1cb9f75d93d15db305627
Franco Aguilera <franco.aguilera@santexgroup.com>
2016-10-01 23:36:44 -0300

    added handlebars as template engine done

--- bootstrap/bootstrap.js
+++ bootstrap/bootstrap.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 
     // view engine setup
     app.set('views', path.join(__dirname, '../views'));
-    app.set('view engine', 'jade');
+    app.set('view engine', 'hbs');
 
     // uncomment after placing your favicon in /public
     //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
