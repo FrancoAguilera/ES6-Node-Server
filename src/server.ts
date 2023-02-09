aeacb38b2d0b0917b8cbc485b3d3a835bd352507
flaalf <flaalf@gmail.com>
2023-02-09 15:40:17 -0300

    routes and 404

--- src/server.ts
+++ src/server.ts
@@ -12,27 +12,29 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// catch 404 and forward to error handler
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const err: ResponseError = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-app.use((err: ResponseError, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err,
-  });
-  next(err);
-});
-
 app.get("/", (req: Request, res: Response) => {
   res.json({
     status: "App running",
     timestamp: Date.now(),
   });
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  res.json({
+    status: "Users page running",
+    timestamp: Date.now(),
+  });
+});
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send("Page not found on the server");
+  next();
+});
+
+app.use((err: ResponseError, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 export default app;
