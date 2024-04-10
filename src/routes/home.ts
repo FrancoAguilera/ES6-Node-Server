21fd831ef1de534d7d81b87810fa3fc573737e73
Franco Aguilera <flaalf@gmail.com>
2024-04-10 18:39:42 -0300

    partial

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,6 @@ import Router, { Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  console.log('alkjsd hflkasjdh flkajsh dflkajhsd')
   res.json({
     status: "App runningo",
     timestamp: Date.now(),
